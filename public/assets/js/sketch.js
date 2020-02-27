1f7b3aa9489febcf996340c06740eb71ede29c46
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-02-27 21:38:20 +0530

    p5 js connected

---
+++ public/assets/js/sketch.js
@@ -0,0 +1,12 @@
+function preload() {
+    console.log("Hello from preload function");
+}
+
+function setup() {
+    console.log("Hello from setup function");
+}
+
+function draw() {
+    console.log("Here we start drawing!!");
+    noLoop();
+}
